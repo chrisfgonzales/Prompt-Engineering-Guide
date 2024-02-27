94b4778a0c76b06b0240136d4550be1421b5a2e7
chrisgfromhawaii@gmail.com <chrisgfromhawaii@gmail.com>
2024-02-27 09:41:10 -0500

    Added 2 files
    Committed by chrisfgonzales on behalf of chrisgfromhawaii@gmail.com

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzODA1MQ/spectral.js?branch_slug=main&token=2076ac2c-92a9-4cfa-a697-62f8922fefc6";
+export default { extends: ruleset };
